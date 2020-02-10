79d02616c03bc8ef174a3d5977e795034c703624
robineco <31357538+robineco@users.noreply.github.com>
2020-02-10 17:53:41 +0100

    some changes

--- game.py
+++ game.py
@@ -14,8 +14,8 @@ WHITE = (255, 255, 255)
 BLUEISH = (17, 86, 143)
 RED = (255, 0, 0)
 
-WIDTH = 400
-HEIGHT = 400
+WIDTH = 600
+HEIGHT = 700
 BAR_WIDTH = 60
 BAR_HEIGHT = 15
 BALL_RADIUS = 10
@@ -28,14 +28,19 @@ score = 0
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption("Unknown Game Name")
 
+pygame.mixer.music.load('music.wav')
+pygame.mixer.music.play(-1)
+
+beep_sound = pygame.mixer.Sound('beep.wav')
+
 ball = pygame.draw.circle(window, BLUEISH, ball_pos, BALL_RADIUS, 0)
-bar = pygame.draw.rect(window, BLACK, Rect(170, 370, BAR_WIDTH, BAR_HEIGHT), 0)
+bar = pygame.draw.rect(window, BLACK, Rect(WIDTH // 2, HEIGHT - 30, BAR_WIDTH, BAR_HEIGHT), 0)
 
 
 def init_ball():
     global ball_pos, ball_velocity
     ball_pos = [WIDTH // 2, HEIGHT // 2]
-    x_velocity = random.uniform(2, 6)
+    x_velocity = random.uniform(3, 8)
     y_velocity = random.uniform(2, 5)
 
     ball_velocity = [x_velocity, y_velocity]
@@ -44,7 +49,7 @@ def init_ball():
 
 def init_bonus():
     global bonus_box
-    for i in range(random.randint(3, 13)):
+    for i in range(random.randint(13, 25)):
         red = random.randint(0, 255)
         green = random.randint(0, 255)
         blue = random.randint(0, 255)
@@ -68,6 +73,7 @@ def final_text(complete):
         text = font.render("Failed", True, RED)
         text_rect = text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
         window.blit(text, text_rect)
+    pygame.mixer.music.stop()
 
 
 def draw_elements():
@@ -82,7 +88,7 @@ def draw_elements():
         ball_velocity[1] *= (-1)
 
     # ball under bar
-    if ball_pos[1] > 380:
+    if ball_pos[1] > HEIGHT - 30:
         final_text(False)
 
     # collision ball with bar
@@ -97,11 +103,12 @@ def draw_elements():
         else:
             ball_velocity[0] *= 1.09
 
-    # collision wall with bonus box
+    # collision ball with bonus box
     for bonus in bonus_box:
         if ball.colliderect(bonus):
             bonus_box.remove(bonus)
             score += 1
+            pygame.mixer.Sound.play(beep_sound)
             ball_velocity[0] *= 1.1
             ball_velocity[1] *= 1.1
 
@@ -133,9 +140,9 @@ def draw_elements():
 def move_bar(left):
     global bar
     if left and bar.x > 0:
-        bar.x -= 10
+        bar.x -= 20
     elif bar.x < WIDTH - BAR_WIDTH:
-        bar.x += 10
+        bar.x += 20
 
 
 def main():
